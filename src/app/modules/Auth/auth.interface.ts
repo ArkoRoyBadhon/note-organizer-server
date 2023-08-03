f041679b79d9e331c66d88a2f82a7b9285987404
Arko <arkoroyb@gmail.com>
2023-08-03 17:09:50 +0600

    project Setup

---
+++ src/app/modules/Auth/auth.interface.ts
@@ -0,0 +1,16 @@
+import { Model, Types } from 'mongoose'
+
+export type UserName = {
+  firstName: string
+  lastName: string
+}
+
+export type IUser = {
+  _id?: Types.ObjectId
+  password: string
+  name: UserName
+  address: string
+  phoneNumber: string
+}
+
+export type UserModel = Model<IUser, Record<string, unknown>>
---
+++ src/app/modules/Auth/auth.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import { IUser, UserModel } from "./auth.interface";
+
+
+const UserSchema = new Schema<IUser, UserModel>(
+    {
+        phoneNumber: {
+          type: String,
+          required: true,
+        },
+        password: {
+          type: String,
+          required: true,
+        },
+        name: {
+          type: {
+            firstName: {
+              type: String,
+              required: true,
+            },
+            lastName: {
+              type: String,
+              required: true,
+            },
+          },
+          required: true,
+        },
+        address: {
+          type: String,
+          required: true,
+        }
+      },
+      {
+        timestamps: true,
+      }
+    )
+    
+    export const User = model<IUser, UserModel>('User', UserSchema)
